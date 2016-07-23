843c5aed1385b49841da87f3204c5ed740afa035
Ivan <pozharskyi@gmail.com>
2016-07-23 09:19:46 +0300

    ADD limiting 3 request per minute

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::group(['prefix' => 'api/v1'], function () {
+Route::group(['prefix' => 'api/v1', 'middleware' => 'throttle:4'], function () {        //v1 - the version of the API, throttle:4 - rate limiting 9 request per minute
     Route::resource('books', 'BooksController', ['except' => [
         'update', 'edit', 'create'
     ]]);
