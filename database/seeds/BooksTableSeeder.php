1a191ced11c653d7883405fb15b514342d1e827f
Ivan <pozharskyi@gmail.com>
2016-07-30 06:37:57 +0300

    ADD seed user_id in books table

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use App\Book;
+use Faker\Factory as Faker;
 
 class BooksTableSeeder extends Seeder
 {
@@ -10,8 +12,17 @@ class BooksTableSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
         factory(Book::class, 50)->create();
+        $faker = Faker::create();
+        $usersId = User::lists('id');
+        //update user_id in books table with user[id] values
+        foreach(range(1,50) as $index){
+            $book = Book::find($index);
+            $book->user_id = $faker->randomElement($usersId->all());
+            $book->save();
+        }
     }
 }
