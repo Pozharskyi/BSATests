9bffa71b2faec18e5768c4f0d29f4e418d658c42
Ivan <pozharskyi@gmail.com>
2016-08-04 23:28:23 +0300

    FIX emails view

--- resources/views/emails/new_book_notify.blade.php
+++ resources/views/emails/new_book_notify.blade.php
@@ -1,6 +1,4 @@
 <p>
     Dear, {{$user->firstname}} {{$user->lastname}}. New book {{ $book->title }} is now available in out library!
 </p>
-<p>
-    <a href="{{ route('books.index') }}">Library</a>
-</p>
+
--- resources/views/emails/refund_book_notify.blade.php
+++ resources/views/emails/refund_book_notify.blade.php
@@ -1,6 +1,3 @@
 <p>
     Dear, {{$user->firstname}} {{$user->lastname}}. Please return the book: {{ $book->title }} to our library.
 </p>
-<p>
-    <a href="{{ route('users.books.index') }}">Your assigned books</a>
-</p>
